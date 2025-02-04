b69c76ad0de6d235d7fae3f94ffd361f47b61ab3
0trifix <otrifix@proton.me>
2025-02-04 15:44:32 +0100

    opdracht 8, en aanpassing opdracht 7

--- Week 1/opdrachten/opdracht7.py
+++ Week 1/opdrachten/opdracht7.py
@@ -1,9 +1,9 @@
 rente = 0.012
 bedrag = float(input("Hoeveel heb je op je rekening? \n"))
 
-jaar_1 = bedrag + bedrag*rente
-jaar_2 = bedrag + (bedrag*rente*2)
-jaar_3 = bedrag + (bedrag*rente*3)
+jaar_1 = round(bedrag + bedrag*rente,2)
+jaar_2 = round(bedrag + (bedrag*rente*2),2)
+jaar_3 = round(bedrag + (bedrag*rente*3),2)
 
 print(f"Na 1 jaar: €{jaar_1}.")
 print(f"Na 2 jaar: €{jaar_2}.")
---
+++ Week 1/opdrachten/opdracht8.py
@@ -0,0 +1,5 @@
+straal = float(input("Geef de straal: "))
+hoogte = float(input("Geef de hoogte: "))
+
+formule = 3.14*(straal**2)*hoogte
+print(round(formule, 1))
