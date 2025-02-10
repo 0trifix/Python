2c669fc8a4138b2b4c27b844a27eab08474efa10
0trifix <otrifix@proton.me>
2025-02-10 15:33:49 +0100

    opdracht 1

---
+++ Week 2/opdrachten/opdracht1.py
@@ -0,0 +1,8 @@
+def ovenen_of_even(getal):
+    if getal % 2 == 0:
+        print("Het getal is even.")
+    else:
+        print("Het getal is oneven.")
+    return
+
+ovenen_of_even(int(input("Geef een getal! ")))
