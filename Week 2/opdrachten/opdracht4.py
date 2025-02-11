0f02bcff1c9ce339b89073dcfeae5c4283af4e50
0trifix <otrifix@proton.me>
2025-02-11 16:26:59 +0100

    oefening 4

---
+++ Week 2/opdrachten/opdracht4.py
@@ -0,0 +1,5 @@
+def naamVeelhoek(aantalHoeken):
+    lijst_namen = ["drie", "vier", "vijf", "zes", "zeven", "acht", "negen", "tien"]
+    print(f"{lijst_namen[aantalHoeken-3]}hoek")
+    return
+naamVeelhoek(4)
