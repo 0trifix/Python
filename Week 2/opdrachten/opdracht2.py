a995a492f2a7f2d6ef473f60e949fa32b57314b8
0trifix <otrifix@proton.me>
2025-02-11 16:13:06 +0100

    opdracht 2

--- Week 2/opdrachten/opdracht2.py
+++ Week 2/opdrachten/opdracht2.py
@@ -1,2 +1,12 @@
-def honden_naar_mensenjaren(leeftijd):
-    
+def honden_naar_mensenjaren(leeftijdMensenJaren):
+    leeftijdHondenJaren = 0
+    if leeftijdMensenJaren == 0:
+        print("Input mag niet nul zijn.")
+        return
+    elif leeftijdMensenJaren <= 2:
+        leeftijdHondenJaren += 10.5 * leeftijdMensenJaren
+    else:
+        leeftijdHondenJaren += (leeftijdMensenJaren - 2) * 4 + 21
+    return leeftijdHondenJaren
+
+print(honden_naar_mensenjaren(int(input("Geef een leeftijd in mensenjaren. "))))
