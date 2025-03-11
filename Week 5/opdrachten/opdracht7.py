396e227c83b806979562225874317664c872b4dc
0trifix <otrifix@proton.me>
2025-03-11 13:40:07 +0100

    opdracht 7

---
+++ Week 5/opdrachten/opdracht7.py
@@ -0,0 +1,17 @@
+import sys
+
+def vraag_getallen():
+    getallen = []
+    while True:
+        getal = input("Voer een getal in: ")
+        if getal == "":
+            break
+        if not getal.isnumeric():
+            print("Dit is geen getal.")
+            continue
+        getallen.append(int(getal))
+    return getallen
+
+def main():
+    getallen = vraag_getallen()
+    print(f"De som van de getallen is: {sum(getallen)}")
