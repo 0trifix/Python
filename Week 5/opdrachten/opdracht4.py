91d895d628199d4977d16e6d28a9be35b5e43e22
0trifix <otrifix@proton.me>
2025-03-11 13:11:52 +0100

    opdracht 4 was niet af

--- Week 5/opdrachten/opdracht4.py
+++ Week 5/opdrachten/opdracht4.py
@@ -15,14 +15,24 @@ def lees_bestand(bestand):
 
 def regelnummers(lines):
     grootste_getal = len(str(len(lines)))
-    for i, line in enumerate(lines):
-        print(f"{i+1:>{grootste_getal}}. {line}", end="")
-    return
+    lines = [f"{i+1:>{grootste_getal}}: {line}" for i, line in enumerate(lines)]
+    return lines
+
+def bestand_schrijven(lines):
+    bestand = input("Welk naam wilt u voor het nieuwe bestand? ")
+    try:
+        with open(bestand, "w") as f:
+            for line in lines:
+                f.write(line)
+    except FileNotFoundError:
+        print("Bestand niet gevonden")
+        quit()
 
 def main():
     bestand = vraag_bestand()
     lines = lees_bestand(bestand)
-    regelnummers(lines)
+    bestand = vraag_bestand
+    bestand_schrijven(regelnummers(lines))
 
 if __name__ == "__main__":
     main()
