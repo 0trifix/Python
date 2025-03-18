9cc1d2a0caf646d318fcd5088684dfefd7e46c4f
0trifix <otrifix@proton.me>
2025-03-18 12:35:22 +0100

    opdracht managment

--- Week 6/main.py
+++ Week 6/main.py
@@ -1,25 +1,77 @@
 import sys
+import json
+import re
 
 def terminal():
     command = input("> ")
-    servers = []
+    serversFile = "servers.json"
+    try:
+        with open(serversFile, "r") as file:
+            if file.read() == "":
+                with open(serversFile, "w") as file:
+                    json.dump([], file)
+    except FileNotFoundError:
+        with open(serversFile, "w") as file:
+            json.dump([], file)
+        
     while command != "exit":
         if command == "add":
-            add()
+            add(serversFile)
         elif command == "remove":
-            remove()
+            remove(serversFile)
         elif command == "list":
-            list()
+            list(serversFile)
+        elif command == "help":
+            help()
         else:
             print("Invalid command")
         command = input("> ")
 
-def add():
+def add(serverFile):
+    name = input("Name: ")
+    ip = input("IP: ")
+    if not checkIP(ip):
+        print("Invalid IP")
+        return
+    server = {
+        "name": name,
+        "ip": ip
+    }
+    with open(serverFile, "r") as file:
+        servers = json.load(file)
+    servers.append(server)
+    with open(serverFile, "w") as file:
+        json.dump(servers, file)
+    
 
+def remove(serverFile):
+    name = input("Name: ")
+    with open(serverFile, "r") as file:
+        servers = json.load(file)
+    for server in servers:
+        if server["name"] == name:
+            servers.remove(server)
+            break
+    with open(serverFile, "w") as file:
+        json.dump(servers, file)
 
-def remove():
+def list(serverFile):
+    with open(serverFile, "r") as file:
+        servers = json.load(file)
+    for server in servers:
+        print(f"{server['name']} - {server['ip']}")
+    
+def help():
+    print("add - Add a server")
+    print("remove - Remove a server")
+    print("list - List all servers")
+    print("exit - Exit the program")
 
-def list():
+def html():
+    pass
+
+def checkIP(ip):
+    return re.match(r"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$", ip) is not None
 
 def main():
     terminal()
