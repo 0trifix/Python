07f101a5dc4b33d235f1c35aa755ffecc055edc5
0trifix <otrifix@proton.me>
2025-03-25 18:06:22 +0100

    het belangrijkste gedaan, nu alleen nog het gedeelte van de posts in orde brengen

--- Week 7/main.py
+++ Week 7/main.py
@@ -1,9 +1,8 @@
 import markdown, frontmatter, os, jinja2
 
 def get_md_files(location):
-    path = os.getcwd() + f"/{location}"
-    files = os.listdir(path)
-    return [path + "/" + file for file in files if file.endswith(".md")]
+    files = os.listdir(f"{location}/")
+    return [location + "/" + file for file in files if file.endswith(".md")]
 
 def get_yaml(file):
     with open(file, 'r') as f:
@@ -15,13 +14,14 @@ def get_content(file):
         post = frontmatter.load(f)
         return post.content
 
-def get_metadata(folder):
+def list_pages(folder):
     pages = []
     for page in get_md_files(folder):
+        metadata = get_yaml(page)
         pages.append({
-            'title': get_yaml(page)["title"],
-            'url': get_yaml(page)["url"],
-            'order': get_yaml(page)["order"]
+            'title': metadata["title"],
+            'url': metadata["url"],
+            'order': metadata["order"]
         })
     pages = sorted(pages, key=lambda x: x['order'])
     return pages
@@ -36,7 +36,7 @@ def render_template(template, pages, content):
 
 def main():
     folder = "pages"
-    pages = get_metadata(folder)
+    pages = list_pages(folder)
     template = get_template()
     for page in get_md_files(folder):
         content = get_content(page)
