37d2a814fb3f456aba37a012923ea49cd63b4fba
0trifix <otrifix@proton.me>
2025-03-25 16:18:54 +0100

    get files functie

--- Week 7/main.py
+++ Week 7/main.py
@@ -1,1 +1,7 @@
-import markdown, frontmatter, os, sys, jinja
+import markdown, frontmatter, os, sys, jinja2
+
+def get_md_files():
+    path = os.getcwd() + "/pages"
+    files = os.listdir(path)
+    return [path + "/" + file for file in files if file.endswith(".md")]
+
