d292d87d92acbd5a36e57e7a05e279249cd39401
0trifix <otrifix@proton.me>
2025-03-25 16:27:12 +0100

    yaml en content functies

--- Week 7/main.py
+++ Week 7/main.py
@@ -5,3 +5,14 @@ def get_md_files():
     files = os.listdir(path)
     return [path + "/" + file for file in files if file.endswith(".md")]
 
+def get_yaml(file):
+    with open(file, 'r') as f:
+        post = frontmatter.load(f)
+        return post.metadata    
+
+def get_file_content(file):
+    with open(file, 'r') as f:
+        return f.read()
+
+def remove_yaml_from_content(content):
+    return content.split("---")[2]
