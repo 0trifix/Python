cd958dd7a8149f20acd5bcb77535eea8ddf61d06
Otrifix <otrifix@proton.me>
2025-04-29 18:01:57 +0200

    ff in trein gewerkt *feels productive*

---
+++ Week 10/opdracht1.py
@@ -0,0 +1,25 @@
+class Drank:
+    def __init__(self, naam, temperatuur=20):
+        self.naam = naam
+        self.temperatuur = temperatuur
+    def __str__(self) -> str:
+        return f"Naam: {self.naam} \nIdeale temperatuur:{self.temperatuur}°C"
+
+class LogFile:
+    def __init__(self, naam: str) -> None:
+        self.bestandsnaam = naam
+        self.bestand = open(naam, "w")
+
+def main():
+    cola = Drank("Cola", 5)
+    koffie = Drank("Koffie", 90)
+    thee = Drank("Thee", 80)
+    water = Drank("Water")
+
+    print(cola)
+    print(koffie)
+    print(thee)
+    print(water)
+
+if __name__ == "__main__":
+    main()
