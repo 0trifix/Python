9d8e1600d9fdcfeffc13670c80e7cfa97b1ce7f6
0trifix <otrifix@proton.me>
2025-04-30 13:39:09 +0200

    PLS LET ME STOP

---
+++ Week 10/opdracht2.py
@@ -0,0 +1,18 @@
+class Boek:
+    def __init__(self, titel, auteur, prijs) -> None:
+        self.titel = titel
+        self.auteur = auteur
+        self.prijs = prijs
+
+class Kast:
+    def __init__(self) -> None:
+        self.boeken = []
+    def voeg_boek_toe(self, boek) -> None:
+        self.boeken.append(boek)
+    def verwijder_boek(self, boek) -> None:
+        self.boeken.remove(boek)
+    def totale_prijs(self) -> float:
+        return sum(boek.prijs for boek in self.boeken)
+    def bevat_boek(self, boek: str) -> bool:
+        return boek in self.boeken
+    def
