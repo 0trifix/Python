4fe154a946c58e3adf8660ecf9a6465cc3178965
Otrifix <otrifix@proton.me>
2025-05-04 14:14:32 +0200

    fuck opdracht 2, mag de tering krijge *spits on ground*

--- Week 10/opdracht2.py
+++ Week 10/opdracht2.py
@@ -1,12 +1,14 @@
 class Boek:
-    def __init__(self, titel, auteur, prijs) -> None:
+    def __init__(self, titel, auteur, prijs, dikte) -> None:
         self.titel = titel
         self.auteur = auteur
         self.prijs = prijs
+        self.dikte = dikte
 
 class Kast:
-    def __init__(self) -> None:
+    def __init__(self, ruimte) -> None:
         self.boeken = []
+        self.ruimte = ruimte
     def voeg_boek_toe(self, boek) -> None:
         self.boeken.append(boek)
     def verwijder_boek(self, boek) -> None:
@@ -15,4 +17,4 @@ class Kast:
         return sum(boek.prijs for boek in self.boeken)
     def bevat_boek(self, boek: str) -> bool:
         return boek in self.boeken
-    def
+    def 
