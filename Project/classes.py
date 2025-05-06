c93a1cfcef42f5e44b85e0229f97453fcf42f2ef
0trifix <otrifix@proton.me>
2025-05-06 09:25:45 +0200

    classes even gefixed terwijl ik class skip

--- Project/classes.py
+++ Project/classes.py
@@ -1,38 +1,44 @@
 class apparaat:
-    def __init__(self, naam, merk, type) -> None:
+    def __init__(self, naam, merk) -> None:
         self.naam = naam
         self.merk = merk
         self.type = type
+        self.status = False
 
 class Lamp(apparaat):
-    def __init__(self, naam, merk, type) -> None:
-        super().__init__(naam, merk, type)
+    def __init__(self, naam, merk) -> None:
+        super().__init__(naam, merk)
+        self.helderheid = 0
         self.type = "lamp"
-
+    
 class Thermostaat(apparaat):
-    def __init__(self, naam, merk, type, temperatuur) -> None:
-        super().__init__(naam, merk, type)
+    def __init__(self, naam, merk, temperatuur) -> None:
+        super().__init__(naam, merk)
         self.temperatuur = temperatuur
+        self.type = "thermostaat"
 
 class Deurslot(apparaat):
-    def __init__(self, naam, merk, type, status) -> None:
-        super().__init__(naam, merk, type)
-        self.status = status
+    def __init__(self, naam, merk) -> None:
+        super().__init__(naam, merk)
+        self.geopend = False
+        self.type = "deurslot"
 
 class Bewegingssensor(apparaat):
-    def __init__(self, naam, merk, type, beweging) -> None:
-        super().__init__(naam, merk, type)
-        self.beweging = beweging
+    def __init__(self, naam, merk) -> None:
+        super().__init__(naam, merk)
+        self.type = "bewegingssensor"
 
 class Rookmelder(apparaat):
-    def __init__(self, naam, merk, type, rook) -> None:
-        super().__init__(naam, merk, type)
-        self.rook = rook
+    def __init__(self, naam, merk) -> None:
+        super().__init__(naam, merk)
+        self.rook_detectie = False
+        self.type = "rookmelder"
 
 class Gordijn(apparaat):
-    def __init__(self, naam, merk, type, open_status) -> None:
-        super().__init__(naam, merk, type)
-        self.open_status = open_status
+    def __init__(self, naam, merk) -> None:
+        super().__init__(naam, merk)
+        self.open = False
+        self.type = "gordijn"
 
 class Kamer:
     def __init__(self, naam) -> None:
@@ -45,7 +51,6 @@ class Woning:
     def __init__(self, naam) -> None:
         self.naam = naam
         self.kamers = []
-
     def voeg_kamer_toe(self, kamer):
         self.kamers.append(kamer)
 
@@ -58,7 +63,6 @@ class SmartHub:
     def __init__(self, naam) -> None:
         self.naam = naam
         self.apparaten = []
-
     def voeg_apparaat_toe(self, apparaat):
         self.apparaten.append(apparaat)
 
@@ -66,14 +70,12 @@ class Logger:
     def __init__(self, naam) -> None:
         self.naam = naam
         self.logs = []
-
     def log(self, bericht):
         self.logs.append(bericht)
 
 class HTML_Generator:
     def __init__(self, naam) -> None:
         self.naam = naam
-
     def genereer_html(self, inhoud):
         with open(f"{self.naam}.html", "w") as f:
             f.write(inhoud)
