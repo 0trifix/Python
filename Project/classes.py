8c5c94232c3a988b0ef3613611761da69267dc2a
0trifix <otrifix@proton.me>
2025-05-07 15:16:07 +0200

    Bewoners ff regelen (starting a slavery business)

--- Project/classes.py
+++ Project/classes.py
@@ -1,4 +1,4 @@
-class apparaat:
+class Apparaat:
     def __init__(self, naam, merk) -> None:
         self.naam = naam
         self.merk = merk
@@ -7,7 +7,7 @@ class apparaat:
     def __str__(self) -> str:
         return f"{self.naam} ({self.merk}) - {self.type} - {'Aan' if self.status else 'Uit'}"
 
-class Lamp(apparaat):
+class Lamp(Apparaat):
     def __init__(self, naam, merk) -> None:
         super().__init__(naam, merk)
         self.helderheid = 0
@@ -24,30 +24,30 @@ class Lamp(apparaat):
     def __str__(self) -> str:
         return f"{super().__str__()} - Helderheid: {self.helderheid}"
     
-class Thermostaat(apparaat):
+class Thermostaat(Apparaat):
     def __init__(self, naam, merk, temperatuur) -> None:
         super().__init__(naam, merk)
         self.temperatuur = temperatuur
         self.type = "thermostaat"
 
-class Deurslot(apparaat):
+class Deurslot(Apparaat):
     def __init__(self, naam, merk) -> None:
         super().__init__(naam, merk)
         self.geopend = False
         self.type = "deurslot"
 
-class Bewegingssensor(apparaat):
+class Bewegingssensor(Apparaat):
     def __init__(self, naam, merk) -> None:
         super().__init__(naam, merk)
         self.type = "bewegingssensor"
 
-class Rookmelder(apparaat):
+class Rookmelder(Apparaat):
     def __init__(self, naam, merk) -> None:
         super().__init__(naam, merk)
         self.rook_detectie = False
         self.type = "rookmelder"
 
-class Gordijn(apparaat):
+class Gordijn(Apparaat):
     def __init__(self, naam, merk) -> None:
         super().__init__(naam, merk)
         self.open = False
@@ -57,8 +57,18 @@ class Kamer:
     def __init__(self, naam) -> None:
         self.naam = naam
         self.apparaten = []
+        self.bewoners = []
     def voeg_apparaat_toe(self, apparaat):
         self.apparaten.append(apparaat)
+    def voeg_bewoner_toe(self, bewoner):
+        self.bewoners.append(bewoner)
+        bewoner.kamer = self
+    def verwijder_bewoner(self, bewoner):
+        if bewoner in self.bewoners:
+            self.bewoners.remove(bewoner)
+            bewoner.kamer = None
+        else:
+            raise ValueError("Bewoner is niet in deze kamer.")
     def __str__(self) -> str:
         apparaten_str = "\n".join([" + " + str(apparaat) for apparaat in self.apparaten])
         return f"{self.naam}\n________\nApparaten:\n{apparaten_str}\n________\n"
